29604f207f2f4c13cb7e000d05a16004f6925012
LeDSantos <santos.leti@hotmail.com>
2020-11-22 23:23:13 -0300

    chmod 777 rodar.sh e ./rodar.sh para executar tudo

---
+++ rodar.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+echo "Calculando pi de 3 formas diferentes"
+make clean
+make all
+
+#numero de interacoes
+N=1000
+
+export OMP_NUM_THREADS=1
+echo -en "\n$OMP_NUM_THREADS threads: SERIAL----------------\n"
+echo -en "\nSIMPSON\n"
+./pi_simpson $N
+
+echo -en "\nMONTECARLO\n"
+./pi_montecarlo $N
+
+#export OMP_NUM_THREADS=3
+NPROC=$(nproc --all)
+export OMP_NUM_THREADS=$NPROC
+
+echo -en "\n$OMP_NUM_THREADS threads: PARALELO--------------\n"
+
+echo -en "\nMONTECARLO\n"
+./pi_montecarlo $N
